1262ca24021679394a983d67a59e67db883dc9de
Kokalagi <127470456+Kokalagi@users.noreply.github.com>
2023-03-20 11:50:04 +0530

    Add files via upload

---
+++ ForLoopeg2.java
@@ -0,0 +1,16 @@
+class ForLoopeg2
+
+{
+    public static void main(String args[])
+    {
+        String names="3RaasRK";
+        
+        
+        
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println(names);
+        }
+    }
+}
+
