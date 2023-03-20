df6d3e0786ffb53f23f3262e7668e056dd01f423
Kokalagi <127470456+Kokalagi@users.noreply.github.com>
2023-03-20 11:40:19 +0530

    Add files via upload

---
+++ ForeachLoop.java
@@ -0,0 +1,16 @@
+class ForeachLoop
+
+{
+    public static void main(String args[])
+    {
+        String[] names= {"Riya","Rohit","Meena","Amit","Sweta"};
+        
+        System.out.println("PRinitng the content of the student name:\n ");
+        
+        for(String result:names)
+        {
+            System.out.println(result);
+        }
+    }
+}
+
