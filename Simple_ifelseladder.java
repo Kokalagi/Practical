f5fe9ef43696c9c986fc342ac91e18887a49f723
Kokalagi <127470456+Kokalagi@users.noreply.github.com>
2023-03-20 09:41:53 +0530

    Add files via upload

---
+++ Simple_ifelseladder.java
@@ -0,0 +1,22 @@
+class Simple_ifelseladder
+{
+public static void main(String args[])
+{
+
+String name = "3RaasRK";
+
+if(name == "Godfather")
+{
+System.out.println("Name is Godfather");
+}
+else if (name == "Tuhin")
+{
+System.out.println("Name is Tuhin");
+}
+else if (name == "Hawa")
+{
+System.out.println("Name is Hawa");
+}
+else{
+System.out.println(name);
+}}}
