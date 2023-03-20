31dc05dcf12db1aa41e2601fea4422c75addac9f
Kokalagi <127470456+Kokalagi@users.noreply.github.com>
2023-03-20 12:54:53 +0530

    Add files via upload

---
+++ Evenno.java
@@ -0,0 +1,22 @@
+class Evenno
+
+{
+public static void main(String args[])
+
+{
+
+int i=0;
+System.out.println("First 10 odd numbers:\n");
+while(i<=10)
+
+{
+   
+System.out.println(i);
+
+i=i+2;
+
+
+}
+
+}
+}
