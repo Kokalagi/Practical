ac5ee885f9fc49b93a05ead1fdfe714d22ab7570
Kokalagi <127470456+Kokalagi@users.noreply.github.com>
2023-03-28 12:37:50 +0530

    Create Object_classEg.java

---
+++ Obj/Object_classEg.java
@@ -0,0 +1,22 @@
+package com;
+
+public class Object_classEg 
+{
+	// Instance Variable / field
+	
+	int S1_no=1;
+	
+	String name="Pallabi";
+	
+	public static void main(String args[])
+	{
+		// Creating the object
+		
+		Object_classEg Obj=new Object_classEg();
+		// Accessing dield with the help of ref variable(Object)
+		
+		System.out.println(Obj.S1_no);
+		System.out.println(Obj.name);
+	}
+
+}
